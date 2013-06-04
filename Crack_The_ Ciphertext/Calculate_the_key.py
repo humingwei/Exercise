52bc73e8a2c588c4db7ffa6fdf8445ce14269793
humingwei@live.cn <humingwei@live.cn>
2013-06-05 00:33:34 +0800

    Add new.
    Add new.

---
+++ Crack_The_ Ciphertext/Calculate_the_key.py
@@ -0,0 +1,12 @@
+﻿data = [int(i) for i in open('cipher1.txt').read().strip().split(",")]
+#print  data
+
+d = {}
+for i in data:
+    d[i] = data.count(i)
+
+keys = sorted(d.iteritems(), key=lambda x:x[1])[-3:]
+print keys
+
+for k,v in keys:
+    print k ^ 32, chr(k ^ 32)
---
+++ Crack_The_ Ciphertext/Readme.txt
@@ -0,0 +1,3 @@
+The text is encrypted by Key. The Key is a word that includes 3 letters. Make all character of the text XOR with the every letter of the Key, then get the Ciphertext.
+
+We need crack the Ciphertext. 
